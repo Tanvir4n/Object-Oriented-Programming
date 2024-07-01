12d6804dcf3da8329eef9ca73430185d2c660469
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-07-02 01:28:28 +0600

    Create HelloWorld.java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,22 @@
+// class creation
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+public: This is Access modifier keyword.
+class: This keyword used to declare a class, here's the class is "HelloWorld".
+
+public static void main(String[] args) it's a method(function)
+public: Access modifier
+static: It indicates that the main method belongs to the class itself rather than to any instance of the class.
+void: This keyword declares nothing would be returned from the method.
+main: This is the name of the method. In Java, main is the conventional name for the method that serves as the entry point of a program.
+String[] args: This specifies the parameter that main accepts. It is an array of strings which allows the passing of command line arguments to the Java program.
+
+System.out.println("Hello World!");
+System: It is the name of Java utility class.
+out: It is an object which bwlongs to system class.
+println: It is utility method name, which is used to send any string to the console.
+*/

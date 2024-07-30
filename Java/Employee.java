760e320d3f0bf56c505e9ecec7cec735e2733e00
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-07-31 01:07:08 +0600

    Create Employee.java

---
+++ Java/Employee.java
@@ -0,0 +1,14 @@
+//package src;
+
+class MyEmployee {
+    int id;
+    String name;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyEmployee emon = new MyEmployee();
+        emon.id = 1448;
+        emon.name = "emonshinthia";
+    }
+}

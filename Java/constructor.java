5285c462932bc92de2e9c0f728f5c7bb2cf48fe6
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-10-05 03:33:13 +0600

    Create constructor.java

---
+++ Java/constructor.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    // No-argument constructor
+    MyClass() {}
+
+    // Main method
+    public static void main(String[] args) {
+        MyClass obj = new MyClass(); // Create an object of MyClass
+        System.out.println("Object of MyClass created: " + obj);
+    }
+}

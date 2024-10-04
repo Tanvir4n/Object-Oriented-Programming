4c25178b604455a546b1499015f80f6e358cda7c
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-10-04 16:21:44 +0600

    Update areaofarectangle.java

--- Java/areaofarectangle.java
+++ Java/areaofarectangle.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
-class Area {
+// Class
+class Area{
     float a;
     float b;
     float area = 0;
-    
-    public float calc_area(float a, float b) {
+
+    // Method (calc_area)
+    public float calc_area(float a, float b){
         return a * b;
     }
+    // Class Close
     
-    public static void main(String[] args) {
+    // Main Method
+    public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         
         System.out.print("Enter the value of a: ");
@@ -17,7 +21,8 @@ class Area {
         
         System.out.print("Enter the value of b: ");
         float b = s.nextFloat();
-        
+
+        // Object
         Area areaObj = new Area();
         
         float area = areaObj.calc_area(a, b);
